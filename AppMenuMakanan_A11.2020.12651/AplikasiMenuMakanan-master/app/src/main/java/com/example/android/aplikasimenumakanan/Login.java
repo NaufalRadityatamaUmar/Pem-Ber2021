1141008c954611a61bd73ebee24ee5373dc656d8
Radityatama <111202012651@mhs.dinus.ac.id>
2022-01-07 19:04:59 +0700

    first commit

---
+++ AppMenuMakanan_A11.2020.12651/AplikasiMenuMakanan-master/app/src/main/java/com/example/android/aplikasimenumakanan/Login.java
@@ -0,0 +1,64 @@
+package com.example.android.aplikasimenumakanan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText username, password;
+    Button login;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.user);
+        password = findViewById(R.id.pass);
+
+        login = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cekLogin();
+            }
+        });
+    }
+
+    private void cekLogin(){
+        String usernameStr = username.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(usernameStr, passwordStr)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(Login.this, "Sukses", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Gagal", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
---
+++ AppMenuMakanan_A11.2020.12651/AplikasiMenuMakanan-master/app/src/main/java/com/example/android/aplikasimenumakanan/MainActivity.kt
@@ -0,0 +1,161 @@
+package com.example.android.aplikasimenumakanan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.text.NumberFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var listFoodChecked: Map<CheckBox, Int>
+    lateinit var listDrinkChecked: Map<CheckBox, Int>
+
+    var localeId = Locale("id", "ID")
+    var formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeId)
+
+    var totalFood: Int = 0
+    var totalDrink: Int = 0
+    var total: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+    fun calculateNow(view: View) {
+        total = 0
+
+        getSelectedFood()
+        getSelectedDrink()
+
+        tv_food_total.text = formatRupiah.format(totalFood)
+        tv_drink_total.text = formatRupiah.format(totalDrink)
+        tv_total.text = formatRupiah.format(total)
+    }
+
+    private fun getSelectedFood() {
+        listFoodChecked = mapOf(
+            cb_food_one to 15000,
+            cb_food_two to 12000,
+            cb_food_three to 16000,
+            cb_food_four to 15000
+        )
+
+        totalFood = 0
+
+        for ((checkedItem, price) in listFoodChecked){
+            if (checkedItem.isChecked){
+                var subTotalFood: Int
+                when(checkedItem){
+                    cb_food_one -> {
+                        if (et_food_one.text.toString() != ""){
+                            subTotalFood = et_food_one.text.toString().toInt() * price
+                            totalFood += subTotalFood
+                        }
+                    }
+
+                    cb_food_two -> {
+                        if (et_food_two.text.toString() != "") {
+                            subTotalFood = et_food_two.text.toString().toInt() * price
+                            totalFood += subTotalFood
+                        }
+                    }
+
+                    cb_food_three -> {
+                        if (et_food_three.text.toString() != "") {
+                            subTotalFood = et_food_three.text.toString().toInt() * price
+                            totalFood += subTotalFood
+                        }
+                    }
+
+                    cb_food_four -> {
+                        if (et_food_four.text.toString() != "") {
+                            subTotalFood = et_food_four.text.toString().toInt() * price
+                            totalFood += subTotalFood
+                        }
+                    }
+                }
+            }
+        }
+        total += totalFood
+    }
+
+    private fun getSelectedDrink() {
+        listDrinkChecked = mapOf(
+            cb_drink_one to 5000,
+            cb_drink_two to 1000,
+            cb_drink_three to 3000,
+            cb_drink_four to 6000
+        )
+
+        totalDrink = 0
+
+        for ((checkedItem, price) in listDrinkChecked){
+            if (checkedItem.isChecked){
+                var subTotalDrink: Int
+                when(checkedItem){
+                    cb_drink_one -> {
+                        if (et_drink_one.text.toString() != "") {
+                            subTotalDrink = et_drink_one.text.toString().toInt() * price
+                            totalDrink += subTotalDrink
+                        }
+                    }
+
+                    cb_drink_two -> {
+                        if (et_drink_two.text.toString() != "") {
+                            subTotalDrink = et_drink_two.text.toString().toInt() * price
+                            totalDrink += subTotalDrink
+                        }
+                    }
+
+                    cb_drink_three -> {
+                        if (et_drink_three.text.toString() != "") {
+                            subTotalDrink = et_drink_three.text.toString().toInt() * price
+                            totalDrink += subTotalDrink
+                        }
+                    }
+
+                    cb_drink_four -> {
+                        if (et_drink_four.text.toString() != "") {
+                            subTotalDrink = et_drink_four.text.toString().toInt() * price
+                            totalDrink += subTotalDrink
+                        }
+                    }
+                }
+            }
+        }
+        total += totalDrink
+    }
+
+    fun payNow(view: View) {
+        if (tv_total.text.toString() != ""){
+            if (et_total_pay.text.toString() != ""){
+                var total_pay = et_total_pay.text.toString().toInt()
+                if (total_pay > total){
+                    total_pay -= total
+                    Toast.makeText(this, "Kembalian anda ${formatRupiah.format(total_pay)}", Toast.LENGTH_SHORT).show()
+                } else {
+                    val makeText = Toast.makeText(this, "Maaf, Uang Anda Tidak Cukup", Toast.LENGTH_SHORT)
+                    makeText.setGravity(Gravity.TOP, 0, 0)
+                    makeText.show()
+                }
+            } else {
+                val makeText = Toast.makeText(this, "Anda Belum Memasukkan Nominal", Toast.LENGTH_SHORT)
+                makeText.setGravity(Gravity.TOP, 0, 0)
+                makeText.show()
+            }
+        } else {
+            val makeText = Toast.makeText(this, "Anda Belum Memilih Menu", Toast.LENGTH_SHORT)
+            makeText.setGravity(Gravity.TOP, 0, 0)
+            makeText.show()
+        }
+    }
+}
